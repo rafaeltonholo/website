397d03f2334087d9f138914540c9f9bd83096f9b
Rafael Tonholo <rafael@tonholo.dev>
2023-08-23 16:03:49 -0300

    chore: add colors and global style

---
+++ src/jsMain/kotlin/dev/tonholo/resume/util/style/GlobalStyle.kt
@@ -0,0 +1,37 @@
+package dev.tonholo.resume.util.style
+
+import dev.tonholo.resume.style.Colors
+import kotlinx.browser.document
+import kotlinx.html.dom.append
+import kotlinx.html.js.style
+import kotlinx.html.unsafe
+
+fun globalStyle() {
+    document.head?.append?.style {
+        unsafe {
+            raw(
+                """
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto Condensed', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: ${Colors.secondary};
+    color: ${Colors.primary};
+  }
+
+  html body #root {
+    width: 100%;
+    height: 100%;
+  }       
+"""
+            )
+        }
+    }
+}
