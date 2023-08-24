3bfd1335518659fb4f67241af1617d4116c3978d
Rafael Tonholo <rafael@tonholo.dev>
2023-08-24 13:42:55 -0300

    fix: react-i18next and i18next-browser-languagedetector not working

--- src/jsMain/kotlin/main.kt
+++ src/jsMain/kotlin/main.kt
@@ -3,6 +3,7 @@ import dev.tonholo.resume.locale.en
 import dev.tonholo.resume.locale.pt
 import dev.tonholo.resume.util.definitions.typescript.i18next.*
 import dev.tonholo.resume.util.definitions.typescript.i18next_browser_languagedetector.I18nextBrowserLanguageDetector
+import dev.tonholo.resume.util.definitions.typescript.i18next_browser_languagedetector.LanguageDetectorModule
 import dev.tonholo.resume.util.definitions.typescript.react_i18next.initReactI18next
 import dev.tonholo.resume.util.style.globalStyle
 import kotlinx.browser.document
@@ -10,9 +11,10 @@ import react.create
 import react.dom.client.createRoot
 
 fun main() {
+
     i18next
         .use(initReactI18next)
-        .use(I18nextBrowserLanguageDetector())
+        .use(LanguageDetectorModule)
         .init<dynamic>(
             options = object : InitOptions {
                 override var resources: Resource?
